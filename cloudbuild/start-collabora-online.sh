d7589de6ef65d598aa77f80f97711437930f4168
amaloun <namalou@hyperlex.fr>
2022-02-24 14:26:31 +0100

    Source secrets before starting the service

--- cloudbuild/start-collabora-online.sh
+++ cloudbuild/start-collabora-online.sh
@@ -3,6 +3,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# Allow to mount secrets on /tmp/.secrets instead of passing them as environment variables
+[ -f /tmp/.secrets ] && source /tmp/.secrets
+
 # Fix domain name resolution from jails
 cp /etc/resolv.conf /etc/hosts /opt/cool/systemplate/etc/
 
